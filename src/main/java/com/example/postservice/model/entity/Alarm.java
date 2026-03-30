1990422c1ae39b8229279aaa18349de21cc05a46
qoiol <hjjj1324@gmail.com>
2026-03-31 01:43:58 +0900

    conflict resolve

--- src/main/java/com/example/postservice/model/entity/Alarm.java
+++ src/main/java/com/example/postservice/model/entity/Alarm.java
@@ -1,45 +1,58 @@
 package com.example.postservice.model.entity;
 
+import com.example.postservice.model.AlarmArgs;
+import com.example.postservice.model.AlarmType;
+import com.vladmihalcea.hibernate.type.json.JsonType;
 import jakarta.persistence.*;
 import lombok.*;
 import org.hibernate.annotations.SQLDelete;
 import org.hibernate.annotations.SQLRestriction;
+import org.hibernate.annotations.Type;
 
 import java.sql.Timestamp;
 import java.time.Instant;
 
 @Entity
-@Table(name = "post")
+@Table(name = "alarms")
 @Builder
-@Data
-@AllArgsConstructor
+@Getter
 @NoArgsConstructor
-@SQLDelete(sql = "update post set deleted_at = now() where id = ?")
+@AllArgsConstructor
+@SQLDelete(sql = "update alarms set deleted_at=now() where id=?")
 @SQLRestriction("deleted_at is null")
-public class Post {
+public class Alarm {
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
-    @Column(name = "id", unique = true, nullable = false)
     private Long id;
+
     @ManyToOne(fetch = FetchType.LAZY)
-    @JoinColumn(name="user_id")
+    @JoinColumn(name = "user_id")
     private User user;
-    @Column(name = "title", nullable = false, length = 200)
-    private String title;
-    @Column(name = "content", columnDefinition = "text not null")
-    private String content;
-    @Column(name="registered_at")
+
+    @Column(name = "alarm_type")
+    @Enumerated(EnumType.STRING)
+    private AlarmType alarmType;
+
+    @Type(value= JsonType.class)
+    @Column(name = "args", columnDefinition = "json")
+    private AlarmArgs args;
+
+    @Column(name = "registered_at")
     private Timestamp registeredAt;
-    @Column(name="updated_at")
+
+    @Column(name = "updated_at")
     private Timestamp updatedAt;
-    @Column(name="deleted_at")
+
+    @Column(name = "deleted_at")
     private Timestamp deletedAt;
+
+//    @Column(name = "read_at")
+//    private Timestamp readAt;
 
     @PrePersist
     public void registeredAt() {
         this.registeredAt = Timestamp.from(Instant.now());
     }
-
     @PreUpdate
     public void updatedAt() {
         this.updatedAt = Timestamp.from(Instant.now());
--- src/main/java/com/example/postservice/model/entity/Token.java
+++ src/main/java/com/example/postservice/model/entity/Token.java
@@ -1,4 +1,4 @@
-package com.example.postservice.model;
+package com.example.postservice.model.entity;
 
 import jakarta.persistence.*;
 import lombok.AllArgsConstructor;
